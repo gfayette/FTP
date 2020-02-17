1ddfad91f417b85d4921c45fd33f4d7679c4f321
G-Fayette <lancaster225@gmail.com>
2020-02-17 11:07:39 -0500

    Squashed the bug

--- server.py
+++ server.py
@@ -1,86 +1,110 @@
 import os
+import struct
+import sys
 from os import path
 import socket
-import time
 from _thread import *
 
-chunk_size = 1024000
 
-
-def start_server():
+def run_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('', 0))
-    # server_socket.bind(('192.168.0.11', 37777))
     server_socket.listen()
     print('The server is listening on', (socket.gethostbyname(socket.gethostname()),
                                          server_socket.getsockname()[1]))
     while True:
-        socket_connection, address = server_socket.accept()
-        print("lmao cd")
-        print('Accepted connection from', address)
-        start_new_thread(service_existing_connection, (socket_connection, address))
+        try:
+            socket_connection, address = server_socket.accept()
+            print('Accepted connection from', address)
+            start_new_thread(service_existing_connection, (socket_connection, address))
+        except KeyboardInterrupt:
+            break
+    print('Closing server.')
+    server_socket.close()
+    sys.exit()
 
 
 def service_existing_connection(socket_connection, address):
     while True:
-        received_data = socket_connection.recv(chunk_size)
-        if received_data:
-            if received_data == b'CLOSE_CONNECTION':
-                print('Closing connection with', address)
-                socket_connection.close()
-                break
-            elif received_data == b'LIST':
-                list_cmd(socket_connection)
-            elif received_data == b'RETRIEVE':
-                retrieve_cmd(socket_connection)
-            elif received_data == b'STORE':
-                store_cmd(socket_connection)
+        received_data = receive_message(socket_connection)
+        if received_data == b'CLOSE_CONNECTION':
+            print('Closing connection with', address)
+            socket_connection.close()
+            break
+        elif received_data == b'LIST':
+            list_cmd(socket_connection)
+        elif received_data == b'RETRIEVE':
+            retrieve_cmd(socket_connection)
+        elif received_data == b'STORE':
+            store_cmd(socket_connection)
 
 
 def list_cmd(socket_connection):
     files = [f for f in os.listdir('.')
              if os.path.isfile(f)]
     for f in files:
-        socket_connection.send(f.encode())
-        time.sleep(.1)
-    socket_connection.send(b'END_TRANSMISSION')
+        send_message(socket_connection, f.encode())
+    send_message(socket_connection, b'END_TRANSMISSION')
 
 
 def retrieve_cmd(socket_connection):
-    file_name = socket_connection.recv(chunk_size).decode()
+    file_name = receive_message(socket_connection).decode()
     if not path.isfile(file_name):
-        socket_connection.send(b'NOT_FOUND')
+        send_message(socket_connection, b'NOT_FOUND')
     else:
         with open(file_name, "rb") as f:
             for piece in read_in_chunks(f):
-                socket_connection.send(piece)
-    time.sleep(1)
-    socket_connection.send(b'END_TRANSMISSION')
+                send_message(socket_connection, piece)
+    send_message(socket_connection, b'END_TRANSMISSION')
 
 
 def store_cmd(socket_connection):
     print("store")
-    file_name = socket_connection.recv(chunk_size).decode()
-    file_data = receive(socket_connection)
+    file_name = receive_message(socket_connection).decode()
+    file_data = receive_in_chunks(socket_connection)
     print("store")
     f = open(file_name, "wb")
     f.write(file_data)
     f.close()
 
 
-def receive(sock):
-    print("rec")
-    received_chunk = sock.recv(chunk_size)
+def send_message(sock, msg):
+    # Prefix each message with a 4-byte length (network byte order)
+    msg = struct.pack('>I', len(msg)) + msg
+    sock.sendall(msg)
+
+
+def receive_in_chunks(sock):
+    received_chunk = receive_message(sock)
     message = b''
     while received_chunk != b'END_TRANSMISSION':
-        print("recchunk", received_chunk)
         message = message + received_chunk
-        received_chunk = sock.recv(chunk_size)
-    print("rec")
+        received_chunk = receive_message(sock)
     return message
 
 
-def read_in_chunks(file_object):
+def receive_message(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = receive_all(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Read the message data
+    return receive_all(sock, msglen)
+
+
+def receive_all(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
+
+
+def read_in_chunks(file_object, chunk_size=1024000):
     while True:
         data = file_object.read(chunk_size)
         if not data:
@@ -89,4 +113,4 @@ def read_in_chunks(file_object):
 
 
 if __name__ == "__main__":
-    start_server()
+    run_server()
